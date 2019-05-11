dc6c8f03f158dcc0233284d2bcfc3c8ef7e00344
Luiz Felipe <raziel_libertino@hotmail.com>
2019-05-11 01:29:09 -0400

    Criação das Entidades

---
+++ QuickBuy.Dominio/Entity/ItemPedido.cs
@@ -0,0 +1,9 @@
+﻿namespace QuickBuy.Dominio.Entity
+{
+    public class ItemPedido
+    {
+        public int Id { get; set; }
+        public int ProdutoId { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
